310f4b0415aca063bcb6e7be182a0d60953a80a8
phaneendra <phani.manthena27@gmail.com>
2023-06-05 13:42:49 +0530

    Scripts added

--- tomcat-installation.sh
+++ tomcat-installation.sh
@@ -12,8 +12,6 @@ cd $DIRECTORY/bin
 sh startup.sh
 
 
-
-
 # #!/bin/bash
 # TARFILE=https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.75/bin/apache-tomcat-9.0.75.tar.gz
 # FILENAME=$(echo $TARFILE | rev | cut -d "/" -f1 | rev)     # Cut command is used
